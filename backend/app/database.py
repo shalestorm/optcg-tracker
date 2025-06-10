74d6f446e9e1f035d23ace08475cee1f4b0d9976
Skyler <mclainskyler@gmail.com>
2025-06-10 19:18:41 -0400

    ya

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://skyler:password@localhost/optcg_stats"
+SQLALCHEMY_DATABASE_URL = os.environ["SQLALCHEMY_DATABASE_URL"]
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
